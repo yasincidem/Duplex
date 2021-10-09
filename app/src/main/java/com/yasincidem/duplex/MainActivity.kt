b5bfaedd1395164589a744dc0addd0607fb89203
Yasin Çidem <yasincidem@gmail.com>
2021-10-09 17:22:44 +0300

    Add splashscreen

--- app/src/main/java/com/yasincidem/duplex/MainActivity.kt
+++ app/src/main/java/com/yasincidem/duplex/MainActivity.kt
@@ -3,11 +3,15 @@ package com.yasincidem.duplex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.accompanist.insets.ProvideWindowInsets
 import com.yasincidem.duplex.ui.theme.DuplexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,10 +19,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DuplexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+        installSplashScreen()
+        initializeUI()
+    }
+}
+
+fun ComponentActivity.initializeUI() {
+    setContent {
+        DuplexTheme {
+            ProvideWindowInsets {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
                     Greeting("Android")
                 }
             }
