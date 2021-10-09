7be02d31c992b74994e1e6d0c8043c0742677f6e
Yasin Çidem <yasincidem@gmail.com>
2021-10-09 14:05:24 +0300

    Add spotless and apply ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,30 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     }
 }
+
+plugins {
+    id("com.diffplug.spotless").version("5.7.0")
+}
+
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension>  {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("**/src/test/resources/**")
+            targetExclude("$buildDir/**/*.kt")
+            targetExclude("bin/**/*.kt")
+
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+
+            ktlint("0.40.0")
+        }
+    }
+}
